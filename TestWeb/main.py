235646bee09ef235d5d0049d501aeb1f19f05d13
Ometh Abeyrathne <ometha12@gmail.com>
2024-06-20 14:55:40 +0530

    v2 code

--- TestWeb/main.py
+++ TestWeb/main.py
@@ -1,27 +1,10 @@
-from flask import Flask, render_template
-import pyfirmata
-# Create a Flask instance
-app = Flask(__name__)
-board = pyfirmata.Arduino('/dev/ttyACM0')
+import os
+x = input("Enter Your Interface :\n1. Arduino\n2. Raspberry Pi\n>")
 
-
-# Define a route and a view function
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
-
-@app.route('/turnOffbutt/<int:value>/<int:status>', methods=['POST'])
-def turnOffbutt(value, status):
-    board.digital[value].write(status)
-    print("Ready :",value, status)
-    return '', 204
-
-# @app.route('/turnOn', methods=['POST'])
-# def turnOnbutt():
-#     board.digital[3].write(1)
-#     print("Ready")
-#     return  '', 205
-
-# Run the Flask app on the local network
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if x == "1":
+    os.system("/home/zerone/duothanEnv/bin/python main_arduino.py")
+elif x == "2":
+    os.system("/home/zerone/duothanEnv/bin/python main_raspberry.py")
+else:
+    print("Invalid Input")
+    exit()
